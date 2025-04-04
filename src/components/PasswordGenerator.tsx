c25b70667622fb5d7007f4d1b22ab470d0dd8d4f
Kelsen23 <nikolozigoreziani@gmail.com>
2025-04-04 17:57:41 +0400

    Implement logic and UI in PasswordGenerator

--- src/components/PasswordGenerator.tsx
+++ src/components/PasswordGenerator.tsx
@@ -1,10 +1,98 @@
+import usePasswordStore from "../store";
 
 const PasswordGenerator = () => {
-  return (
-    <div>
-      Hello!   
-    </div>
-  )
-}
+  const {
+    length,
+    setLength,
+    includeNumbers,
+    toggleNumbers,
+    includeSymbols,
+    toggleSymbols,
+    includeUppercase,
+    toggleUppercase,
+    includeLowercase,
+    toggleLowercase,
+    generatedPassword,
+    generatePassword,
+  } = usePasswordStore();
 
-export default PasswordGenerator
+  const handleGeneratePassword = () => generatePassword();
+
+  return (
+    <div className="p-8 w-[40rem] mx-auto bg-white shadow-lg rounded-lg">
+      <h1 className="text-2xl font-bold mb-4">Password Generator</h1>
+
+      <div className="flex flex-col gap-4">
+        <div>
+          <label
+            htmlFor="length"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Password Length
+          </label>
+
+          <input
+            type="number"
+            id="length"
+            value={length}
+            onChange={(e) => setLength(+e.target.value)}
+            min={4}
+            max={64}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+          />
+        </div>
+
+        <div className="flex items-center">
+          <input
+            type="checkbox"
+            checked={includeNumbers}
+            onChange={toggleNumbers}
+          />
+          <label className="ml-2 text-sm">Include Numbers</label>
+        </div>
+
+        <div className="flex items-center">
+          <input
+            type="checkbox"
+            checked={includeSymbols}
+            onChange={toggleSymbols}
+          />
+          <label className="ml-2 text-sm">Include Symbols</label>
+        </div>
+
+        <div className="flex items-center">
+          <input
+            type="checkbox"
+            checked={includeUppercase}
+            onChange={toggleUppercase}
+          />
+          <label className="ml-2 text-sm">Include Uppercase</label>
+        </div>
+
+        <div className="flex items-center">
+          <input
+            type="checkbox"
+            checked={includeLowercase}
+            onChange={toggleLowercase}
+          />
+          <label className="ml-2 text-sm">Include Lowercase</label>
+        </div>
+
+        <button
+          onClick={handleGeneratePassword}
+          className="px-4 py-2 mt-4 cursor-pointer bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+        >
+          Generate Password
+        </button>
+
+        {generatedPassword !== "" && (
+          <div className="mt-4 p-4 bg-gray-100 rounded-lg">
+            <p className="text-lg break-all">{generatedPassword}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default PasswordGenerator;
